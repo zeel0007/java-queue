5b34e5abbd3a0331098081a8f4be861d2ebeec02
zeel0007 <zeelbhanderi03@gmail.com>
2025-11-01 11:04:26 +0530

    Added NonRepInStram.java file

---
+++ NonRepInStram.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NonRepInStram {
+    public static void printNonRepet(String str){
+        int freq []= new int[26];
+
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+
+            while (!q.isEmpty() && freq[q.peek()-'a']>1) {
+                q.remove();
+            }
+            if(q.isEmpty()){
+                System.out.println(-1);
+            }
+            else{
+                System.out.println(q.peek());
+            }
+        }
+        System.out.println();
+            
+       
+    }
+   public static void main(String[] args) {
+    String str = "aabccxb";
+    printNonRepet(str);
+   } 
+}
